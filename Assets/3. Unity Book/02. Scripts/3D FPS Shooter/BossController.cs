14abb48e803dfdd1da0640846e13a5fb8c36531c
sbc605 <sbc605@naver.com>
2025-07-24 21:01:31 +0900

    Nav Mesh: Link, Modifier, Obstacle, Plus 2D / Agent Link Mover

---
+++ Assets/3. Unity Book/02. Scripts/3D FPS Shooter/BossController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BossController : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public Transform target;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+        agent.SetDestination(target.position);
+    }
+}
--- Assets/3. Unity Book/02. Scripts/3D FPS Shooter/FPSPlayerFire.cs
+++ Assets/3. Unity Book/02. Scripts/3D FPS Shooter/FPSPlayerFire.cs
@@ -1,35 +1,57 @@
+using System.Collections;
+using TMPro;
+using UnityEditor.Build;
 using UnityEngine;
 
 public class FPSPlayerFire : MonoBehaviour
 {
+    private enum WeaponMode { Normal, Sniper }
+    private WeaponMode wMode;
+
     public GameObject firePosition;
     public GameObject bombFactory;
     public GameObject bulletEffect;
     private Animator anim;
     private ParticleSystem ps;
 
+    public TextMeshProUGUI wModeText;
+    public GameObject[] eff_Flash;
+
     public float throwPower = 15f;
     public int weaponPower = 5;
+
+    private bool ZoomMode = false;
 
 
     void Start()
     {
         anim = GetComponentInChildren<Animator>();
         ps = bulletEffect.GetComponent<ParticleSystem>();
+
+        wMode = WeaponMode.Normal;
     }
 
     void Update()
     {
         if (FPSGameManager.Instance.gState != FPSGameManager.GameState.Run)
             return;
-        
+
+        LeftClick();
+        RightClick();
+        AlphaKeypad();
+    }
+
+    void LeftClick()
+    {
         if (Input.GetMouseButtonDown(0)) // 마우스 왼쪽 버튼 클릭
         {
             if (anim.GetFloat("MoveMotion") == 0)
                 anim.SetTrigger("Attack");
-            
+
+            StartCoroutine(ShootEffectOn(0.05f));
+
             Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
-            RaycastHit hitInfo =  new RaycastHit();
+            RaycastHit hitInfo = new RaycastHit();
 
             if (Physics.Raycast(ray, out hitInfo))
             {
@@ -47,14 +69,67 @@ public class FPSPlayerFire : MonoBehaviour
                 }
             }
         }
-        
+    }
+
+    void RightClick()
+    {
         if (Input.GetMouseButtonDown(1)) // 마우스 오른쪽 버튼 클릭
         {
-            GameObject bomb = Instantiate(bombFactory);
-            bomb.transform.position = firePosition.transform.position;
+            switch (wMode)
+            {
+                case WeaponMode.Normal: // 일반 모드일 때 마우스 오른쪽 -> 수류탄 투척
+                    GameObject bomb = Instantiate(bombFactory);
+                    bomb.transform.position = firePosition.transform.position;
 
-            Rigidbody rb = bomb.GetComponent<Rigidbody>();
-            rb.AddForce(Camera.main.transform.forward * throwPower, ForceMode.Impulse);
+                    Rigidbody rb = bomb.GetComponent<Rigidbody>();
+                    rb.AddForce(Camera.main.transform.forward * throwPower, ForceMode.Impulse);
+                    break;
+
+
+                case WeaponMode.Sniper: // 저격 모드일 때 마우스 오른쪽 -> 확대/축소 조준경
+                    /* if (!ZoomMode)
+                    {
+                        Camera.main.fieldOfView = 15f;
+                        ZoomMode = true;
+                    }
+                    else
+                    {
+                        Camera.main.fieldOfView = 60f;
+                        ZoomMode = false;
+                    } */
+
+                    float fov = ZoomMode ? 60f : 15f;
+                    Camera.main.fieldOfView = fov;
+                    ZoomMode = !ZoomMode;
+                    break;
+            }
         }
+    }
+
+    void AlphaKeypad()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) // qwer 위 1 // Keypad1은 오른쪽 키패드넘버
+        {
+            wMode = WeaponMode.Normal;
+            Camera.main.fieldOfView = 60f;
+
+            wModeText.text = "Normal Mode";
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            wMode = WeaponMode.Sniper;
+
+            wModeText.text = "Sniper Mode";
+        }
+    }
+
+    IEnumerator ShootEffectOn(float duration)
+    {
+        int num = Random.Range(0, eff_Flash.Length - 1);
+        eff_Flash[num].SetActive(true);
+
+        yield return new WaitForSeconds(duration);
+
+        eff_Flash[num].SetActive(false);
     }
 }
